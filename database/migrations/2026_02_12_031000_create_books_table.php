6e295deb828c25a2f90ac1d110ed6c3d0dec9320
Nazwaaaaa <nyctophile2434@gmail.com>
2026-03-05 10:08:16 +0700

    first commit

---
+++ database/migrations/2026_02_12_031000_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id(); // integer, auto increment, primary key
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('title'); // judul buku
+            $table->string('author'); // penulis
+            $table->integer('year'); // tahun terbit
+            $table->integer('stock')->default(1); // jumlah buku tersedia
+            $table->text('description')->nullable(); // keterangan tambahan
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
